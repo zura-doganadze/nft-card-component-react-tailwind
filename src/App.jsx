84b07ed409d68cd97a5e17721c1437dab5280862
Zura Doghanadze <zuradoghanadze@gmail.com>
2023-09-17 22:45:47 +0400

    add img component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
 import "./App.css";
+import Equilib from "./img/image-equilibrium.jpg";
+import Icon from "./img/icon-view.svg";
+import Ethereum from "./img/icon-ethereum.svg";
+import Clock from "./img/icon-clock.svg";
+import Avatar from "./img/image-avatar.png";
 
 function App() {
   return (
     <>
-      <h1 className="text-6xl font-bold underline">Hello world!</h1>
+      <div className="flex justify-center mt-6 ">
+        <div className="flex flex-col justify-center px-6 bg-card-bg max-w-lg w-full py-6 rounded-xl ">
+          <div className="relative   cursor-pointer rounded-md overflow-hidden">
+            <img src={Equilib} alt="" />
+            <div
+              className="text-white z-10 absolute top-0 w-full h-full flex items-center
+                          justify-center bg-hover-bg overflow-hidden rounded-md bg-fixed
+                          opacity-0 transition duration-300 ease-in-out hover:opacity-100"
+            >
+              <img src={Icon} alt="" />
+            </div>
+          </div>
+          
+        </div>
+      </div>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        "card-bg": "hsl(216, 50%, 16%)",
+        "hover-bg": "hsl(178, 100%, 50%, 60%)",
+        "line-color": "hsl(215, 32%, 27%)"
+
+      },
+      textColor: {
+        "text-hover": "hsl(178, 100%, 50%)",
+        "price-color": "hsl(178, 100%, 50%)",
+        "span-color": "hsl(215, 51%, 70%)",
+        
+      }
+    },
   },
   plugins: [],
 };
