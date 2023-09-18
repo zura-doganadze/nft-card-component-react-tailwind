28bb69b3c1d2c59594c3f9bbf66b485ad0a91d80
Zura Doghanadze <zuradoghanadze@gmail.com>
2023-09-18 17:05:44 +0400

    add the final details and responsive

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ import Avatar from "./img/image-avatar.png";
 function App() {
   return (
     <>
-      <div className="flex justify-center mt-6 ">
-        <div className="flex flex-col justify-center px-6 bg-card-bg max-w-lg w-full py-6 rounded-xl ">
+      <div className="flex flex-col items-center justify-center mt-6 ">
+        <div className="flex flex-col justify-center px-6 bg-card-bg max-w-lg w-full pt-6 rounded-xl ">
           <div className="relative   cursor-pointer rounded-md overflow-hidden">
             <img src={Equilib} alt="" />
             <div
@@ -20,7 +20,57 @@ function App() {
               <img src={Icon} alt="" />
             </div>
           </div>
-          
+          <div className="my-6">
+            <div className="flex flex-col items-start text-left">
+              <h1 className="font-sans text-2xl font-semibold text-white cursor-pointer hover:text-text-hover">
+                Equilibrium #3429
+              </h1>
+              <p className="mt-4 pr-1 font-sans text-lg text-span-color">
+                Our Equilibrium collection promotes balance and calm.
+              </p>
+              <div className="flex w-full justify-between mt-4 ">
+                <div className="flex w-max gap-x-2">
+                  <img src={Ethereum} alt="eterium img" />
+                  <span className="text-lg inline-block font-semibold text-price-color">
+                    0.041 ETH
+                  </span>
+                </div>
+                <div className="flex w-max gap-x-2 ">
+                  <img src={Clock} alt="clock img" />
+                  <span className="text-lg inline-block font-semibold text-span-color">
+                    3 days left
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div className="h-[2px] bg-line-color mt-6"></div>
+            <div className="flex items-center justify-start pt-8">
+              <img
+                src={Avatar}
+                alt="img"
+                className="h-[54px] w-[54px] rounded-full border border-white mr-4"
+              />
+              <p className="text-lg text-span-color">
+                Creation of
+                <span className="text-white cursor-pointer hover:text-text-hover ml-2">
+                  Jules Wyvern
+                </span>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <p className="text-lg text-span-color">
+            Challenge by
+            <a className="text-adrees-t-color mx-1 cursor-pointer border-b-2 border-blue-900">
+              Frontend Mentor.
+            </a>
+            Coded by
+            <a className="text-adrees-t-color mx-1 cursor-pointer border-b-2 border-blue-900">
+              Thomas Sankara
+            </a>
+            .
+          </p>
         </div>
       </div>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,12 @@ export default {
         "card-bg": "hsl(216, 50%, 16%)",
         "hover-bg": "hsl(178, 100%, 50%, 60%)",
         "line-color": "hsl(215, 32%, 27%)"
-
       },
       textColor: {
         "text-hover": "hsl(178, 100%, 50%)",
         "price-color": "hsl(178, 100%, 50%)",
         "span-color": "hsl(215, 51%, 70%)",
-        
+        "adrees-t-color": "hsl(228, 45%, 44%)"
       }
     },
   },
